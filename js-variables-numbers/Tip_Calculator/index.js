8196eabdf13393c4dacaf5461a9773468e38f915
Kristoffer Jans <jans.kiki@gmail.com>
2024-08-01 13:21:28 +0200

    created tip calculator in js

--- js-variables-numbers/Tip_Calculator/index.js
+++ js-variables-numbers/Tip_Calculator/index.js
@@ -1,6 +1,13 @@
-const mealCost = 20
-const
+const mealCost = 20;
+const tipPercentage = 0.2;
 
-console.log(mealCost)
+const tipAmount = mealCost * tipPercentage;
+const totalCost = mealCost + tipAmount;
 
-console.log(hello)
+console.log("The price of your meal is: " + mealCost + "€");
+
+console.log("We expect you to tip at least 20%.");
+
+console.log(
+  "The total amount of your meal including tip is: " + totalCost + "€"
+);
