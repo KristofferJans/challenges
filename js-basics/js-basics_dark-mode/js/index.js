85987877ce6f495e3fad229c6a8e02ec3f73b761
Kristoffer Jans <jans.kiki@gmail.com>
2024-08-02 15:02:30 +0200

    solved challenge

--- js-basics/js-basics_dark-mode/js/index.js
+++ js-basics/js-basics_dark-mode/js/index.js
@@ -2,5 +2,23 @@ console.clear();
 
 const bodyElement = document.querySelector('[data-js="body"]');
 
-const button = document.querySelector('[data-js="dark-mode-button"]');
-button.addEventListener("click", () => {});
+const darkModeButton = document.querySelector('[data-js="dark-mode-button"]');
+const lightModeButton = document.querySelector('[data-js="light-mode-button"]');
+const toggleModeButton = document.querySelector('[data-js="toggle-button"]');
+
+darkModeButton.addEventListener("click", () => {
+  bodyElement.classList.remove("light");
+  bodyElement.classList.add("dark");
+  console.log("switched to dark mode");
+});
+
+lightModeButton.addEventListener("click", () => {
+  bodyElement.classList.remove("dark");
+  bodyElement.classList.add("light");
+  console.log("switched to light mode");
+});
+
+toggleModeButton.addEventListener("click", () => {
+  bodyElement.classList.toggle("dark");
+  console.log("switched to toggle mode");
+});
