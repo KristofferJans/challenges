ef5ea618dd53f1cda47a309680ebc059397bc608
Kristoffer Jans <jans.kiki@gmail.com>
2024-08-02 13:07:29 +0200

    added first line of code

--- js-basics/js-basics_dark-mode/js/index.js
+++ js-basics/js-basics_dark-mode/js/index.js
@@ -1,3 +1,6 @@
 console.clear();
 
 const bodyElement = document.querySelector('[data-js="body"]');
+
+const button = document.querySelector('[data-js="dark-mode-button"]');
+button.addEventListener("click", () => {});
