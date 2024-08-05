7d8ab73fe70b44626a82c1c1e9f6db42fe8198d4
Kristoffer Jans <jans.kiki@gmail.com>
2024-08-05 23:03:39 +0200

    solved task 1

--- js-functions/js-functions_pizza-analyzer/js/index.js
+++ js-functions/js-functions_pizza-analyzer/js/index.js
@@ -9,15 +9,30 @@ const outputSection = document.querySelector('[data-js="output-section"]');
 const output = document.querySelector('[data-js="output"]');
 
 pizzaInput1.addEventListener("input", () => {
-  // write your code here
+  const pizzaSize1 = pizzaInput1.value;
+  const pizzaSize2 = pizzaInput2.value;
+  calculatePizzaGain(pizzaSize1, pizzaSize2);
 });
 
 pizzaInput2.addEventListener("input", () => {
-  // write your code here
+  const pizzaSize1 = pizzaInput1.value;
+  const pizzaSize2 = pizzaInput2.value;
+  calculatePizzaGain(pizzaSize1, pizzaSize2);
 });
 
 // Task 1
 // define the function calculatePizzaGain here
+
+function calculatePizzaGain(diameter1, diameter2) {
+  let radius1 = diameter1 / 2;
+  let radius2 = diameter2 / 2;
+  let area1 = Math.PI * Math.pow(radius1, 2);
+  let area2 = Math.PI * Math.pow(radius2, 2);
+  let pizzaGain = ((area2 - area1) / area1) * 100;
+  console.log(Math.round(pizzaGain));
+}
+
+calculatePizzaGain(pizzaSize1, pizzaSize2);
 
 // Task 2
 // define the function updatePizzaDisplay here
