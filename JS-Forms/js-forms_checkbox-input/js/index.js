7dd83bc3e11c218f430ebee2918703a17e9e2b40
Kristoffer Jans <jans.kiki@gmail.com>
2024-08-08 00:14:18 +0200

    started working on the challenge

--- JS-Forms/js-forms_checkbox-input/js/index.js
+++ JS-Forms/js-forms_checkbox-input/js/index.js
@@ -1,5 +1,23 @@
 console.clear();
 
+// # Checkbox Input
+
+// TOS is short for Terms of Service. Here, the `alert()` in the submit event handler should only be called if the `tos` checkbox has been checked.
+
+// > 💡 You can use an early `return;` in an if statement to prevent the `alert()` from being called.
+
+// In addition, the error message below the checkbox should only be displayed if the checkbox is unchecked. The message should disappear as soon as the user checks the checkbox - and it should reappear as soon as the user unchecks the checkbox. Use the given functions `showTosError()` and `hideTosError()`.
+
+// Can you make the error message hidden initially (before the form is submitted)?
+
+// ## Bonus: Success message
+
+// Add this HTML code to the [`index.html`](./index.html) below the form. Write JavaScript code to only show the div if the submission was valid (`tos` checkbox was checked). Like the error message, it should also be hidden initially.
+
+// ```html
+// <div data-js="success" class="success">Complaint successfully submitted!</div>
+// ```
+
 const form = document.querySelector('[data-js="form"]');
 const tosError = document.querySelector('[data-js="tos-error"]');
 const tosCheckbox = document.querySelector('[data-js="tos"]');
@@ -14,6 +32,11 @@ function showTosError() {
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
+
+  if (!tosCheckbox.checked) {
+    showTosError();
+    return;
+  }
 
   // --v-- write your code here --v--
 
