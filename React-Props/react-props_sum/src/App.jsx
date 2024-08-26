7b59ec18a3675191a7b0cf696a9496a902a5b890
Kristoffer Jans <jans.kiki@gmail.com>
2024-08-26 11:07:38 +0200

    React Props: Calculating the Sum

--- React-Props/react-props_sum/src/App.jsx
+++ React-Props/react-props_sum/src/App.jsx
@@ -1,5 +1,13 @@
 import "./App.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return <Sum valueA={1} valueB={2} />;
+}
+
+function Sum({ valueA, valueB }) {
+  return (
+    <h1>
+      {valueA} + {valueB} = {valueA + valueB}
+    </h1>
+  );
 }
