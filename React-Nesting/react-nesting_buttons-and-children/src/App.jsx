d8716c3191755c0fe244e13a3e5b3ec5e9d091aa
Kristoffer Jans <jans.kiki@gmail.com>
2024-08-26 15:30:42 +0200

    React Nesting: Buttons and Children

--- React-Nesting/react-nesting_buttons-and-children/src/App.jsx
+++ React-Nesting/react-nesting_buttons-and-children/src/App.jsx
@@ -3,18 +3,18 @@ import "./App.css";
 export default function App() {
   return (
     <main>
-      <Button />
-      <Button />
-      <Button />
-      <Button />
+      <Button>I like turtles!</Button>
+      <Button>I like trains!</Button>
+      <Button>This is a button!</Button>
+      <Button>This is also a button!</Button>
     </main>
   );
 }
 
-function Button() {
+function Button({ children }) {
   return (
     <button className="button" type="button">
-      Click me!
+      {children}
     </button>
   );
 }
