7e05495c1b8c9f8d525672796e2d647548b4a530
Kristoffer Jans <jans.kiki@gmail.com>
2024-08-26 11:20:04 +0200

    React Props: Smiley

--- React-Props/react-props_smiley/src/App.jsx
+++ React-Props/react-props_smiley/src/App.jsx
@@ -1,5 +1,14 @@
 import "./App.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return (
+    <div>
+      <Smiley isHappy />
+      <Smiley />
+    </div>
+  );
+}
+
+function Smiley({ isHappy }) {
+  return <div>{isHappy ? `😁` : `😢`}</div>;
 }
