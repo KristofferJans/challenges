f53f912a5e9262e3a5826e14d47515bbc36957d0
Kristoffer Jans <jans.kiki@gmail.com>
2024-08-27 15:35:04 +0200

    added components

---
+++ React-Project-Setup/vite-project_journal-app/components/Entries.jsx
@@ -0,0 +1,12 @@
+export default function Entries() {
+  return (
+    <nav>
+      <p>
+        All entries <span>3</span>
+      </p>
+      <p>
+        Favorites <span>1</span>
+      </p>
+    </nav>
+  );
+}
