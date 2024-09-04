dc9f382c47c4bf9bd4680c696a7f00e1de157974
Kristoffer Jans <jans.kiki@gmail.com>
2024-09-04 17:24:28 +0200

    add dynamic routing on overview page and add random page button

--- Next-JS/nextjs-dynamic-routes_lotr-app-dynamic-routes/pages/volumes/index.js
+++ Next-JS/nextjs-dynamic-routes_lotr-app-dynamic-routes/pages/volumes/index.js
@@ -1,26 +1,31 @@
 import Link from "next/link";
 import { introduction } from "../../lib/data";
+import { volumes } from "../../lib/data.js";
+import Router from "next/router";
+
+function GetRandomElement(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
 
 export default function Volumes() {
+  function AddRandomElement() {
+    const randomElement = GetRandomElement(volumes);
+    Router.push(`/volumes/${randomElement.slug}`);
+  }
   return (
     <>
       <h1>The Lord of the Rings</h1>
       <p>{introduction}</p>
       <ul>
-        <li>
-          <Link href="/volumes/the-fellowship-of-the-ring">
-            The Fellowship of the Ring
-          </Link>
-        </li>
-        <li>
-          <Link href="/volumes/the-two-towers">The Two Towers</Link>
-        </li>
-        <li>
-          <Link href="/volumes/the-return-of-the-king">
-            The Return of the King
-          </Link>
-        </li>
+        {volumes.map((volume) => (
+          <li key={volume.slug}>
+            <Link href={`/volumes/${volume.slug}`}>{volume.title}</Link>
+          </li>
+        ))}
       </ul>
+      <button type="button" onClick={AddRandomElement}>
+        Random Movie
+      </button>
     </>
   );
 }
