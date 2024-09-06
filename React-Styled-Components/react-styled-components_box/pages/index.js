d74c490fe4cc20b74cd7b656545f4e997c81f36b
Kristoffer Jans <jans.kiki@gmail.com>
2024-09-06 11:36:15 +0200

    solved

--- React-Styled-Components/react-styled-components_box/pages/index.js
+++ React-Styled-Components/react-styled-components_box/pages/index.js
@@ -1,10 +1,18 @@
+import styled from "styled-components";
 import BoxWithClassName from "../components/BoxWithClassName/BoxWithClassName.js";
+import BoxWithStyledComponents from "@/components/BoxWithStyledComponents.js";
 
 export default function HomePage() {
   return (
-    <div>
+    <StyledBoxes>
       <BoxWithClassName />
       <BoxWithClassName isBlack />
-    </div>
+      <BoxWithClassName />
+      <BoxWithStyledComponents $isBlack />
+    </StyledBoxes>
   );
 }
+
+const StyledBoxes = styled.div`
+  display: flex;
+`;
